10fa281ce6cb85652fc97ef39f6c8249bfe82354
Oleg Chepelev <yorkblansh@gmail.com>
2022-04-12 23:22:02 +0300

    ✨ feat(index.ts): finally set up good start-service-function
    organizes good looking function

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,21 @@
-import { onEvent } from './app/net/onEvent'
-import { TaskManager } from './app/Tasker'
-import { TaskEvents } from './app/vars-ts/net'
 import { ConvertEnum } from './app/vars-ts/fns'
-import { every } from 'node-cron-expression'
-import CronJobManager from 'cron-job-manager'
-import { ITMmethods } from './app/Tasker'
+import { TaskEventsEnum } from './app/vars-ts/cron-task-manager'
+import { localTieBus } from './app/TieBus'
+import { newTaskManager } from './app/TaskManager'
 
 const startService = () => {
-	// const cron2 = every(2).minutes().
+	const taskEventsArray = ConvertEnum(TaskEventsEnum).toObjectArray()
+	const DevicesTaskManager = newTaskManager({
+		task_manager_key: 'device_schedules',
+		cronexp: '0/5 * * * * *',
+	})
+	const TieBus = localTieBus()
 
-	const cronexp = '*/10 * * * * *'
-	const TaskEventsArray = ConvertEnum(TaskEvents).toArray_Of_OBJECTS()
-	const taskManager = new TaskManager('device_schedules', cronexp.toString())
-
-	console.dir('device_schedules started')
-	TaskEventsArray.map(({ value: taskEvent }) => onEvent(taskEvent, taskProps => taskManager[taskEvent](taskProps)))
-	// taskManager.perf['ADD_TASK']({
-	// 	job_key: 'tt',
-	// 	cron_exp: '*/2 * * * * *',
-	// 	task: 'tt',
-	// })
-	// taskManager.perf['START_TASK']({ job_key: 'tt' })
-	// taskManager.addTASK({
-	// 	job_key: 'tt',
-	// 	cron_exp: '*/2 * * * * *',
-	// 	task: 'tt',
-	// })
+	taskEventsArray.map(({ value: taskEvent }) => {
+		TieBus.onEvent(taskEvent, taskProps => {
+			DevicesTaskManager[taskEvent](taskProps)
+		})
+	})
 }
 
 startService()
