955d7ee7bbbaff5424f8a832d2a7b39697bdc916
Oleg Chepelev <yorkblansh@gmail.com>
2022-04-26 17:38:05 +0300

    ✨ feat(TieBus): perform new sockets
    perform two new sockets for: timetable, executor

--- src/app/TieBus.ts
+++ src/app/TieBus.ts
@@ -3,13 +3,15 @@ import { io } from 'socket.io-client'
 import { TaskEventsEnum, TaskProps } from './vars-ts/node-timetable'
 import { TieBus } from './vars-ts/tiebus'
 
-const socket = io('localhost/tasker')
+const socket_timetable = io('localhost/socket.timetable')
+const socket_executor = io('localhost/socket.executor')
 
 export const localTieBus = () =>
 	({
 		onEvent: (event: keyof typeof TaskEventsEnum, cb: (task_obj: TaskProps) => void) => {
+			socket_timetable.on(event, task_obj => cb(task_obj))
 		},
 		emit: (event: keyof typeof TaskEventsEnum, task_obj: TaskProps) => {
+			socket_executor.emit(event, task_obj)
 		},
-		UNSAFE_socket: socket,
-	} as TieBus<typeof socket>)
+	} as TieBus)
