cd815061d8a7bc84907b2e2b2e5f2ed6a6d0849b
Oleg Chepelev <yorkblansh@gmail.com>
2022-04-26 17:58:23 +0300

    ✨ feat(TaskManager): reformat

--- src/app/TaskManager.ts
+++ src/app/TaskManager.ts
@@ -1,29 +1,53 @@
 /* eslint-disable prefer-const */
 import CronJobManager from 'cron-job-manager'
-import { TaskManager } from './vars-ts/node-timetable'
+import { TaskManager, TaskProps } from './vars-ts/node-timetable'
+import { TieBus } from './vars-ts/tiebus'
 
 interface TaskManagerProps {
 	task_manager_key: string
 	cronexp?: string
+	exec: TieBus['emit']
+	logger: any
 }
 
-export const TaskManagerInstance = ({ cronexp, task_manager_key }: TaskManagerProps): TaskManager => {
-	const manager = new CronJobManager(
-		task_manager_key,
-		cronexp ? cronexp : '0/5 * * * * *',
-		() => console.dir(`TaskManager ${task_manager_key} still works!`),
-		{
-			// see https://www.npmjs.com/package/cron for all available
-			start: true,
-			timezone: 'Europe/Moscow',
-			onComplete: () => console.log('a_key_string_to_call_this_job has stopped....'),
-		},
-	)
+export class TaskManagerInstance implements TaskManager {
+	private manager: CronJobManager
+	private logger: any
+	private exec: TieBus['emit']
 
-	return {
-		ADD_TASK: ({ job_key, cron_exp, task }) => manager.add(job_key, cron_exp, () => console.dir(task)),
-		START_TASK: ({ job_key }) => manager.start(job_key),
-		STOP_TASK: ({ job_key }) => manager.stop(job_key),
-		UPDATE_TASK: ({ cron_exp, job_key, task }) => manager.update(job_key, cron_exp, () => console.dir(task)),
-	} as TaskManager
+	constructor({ cronexp, task_manager_key, exec, logger }: TaskManagerProps) {
+		this.manager = new CronJobManager(
+			task_manager_key,
+			cronexp ? cronexp : '0/5 * * * * *',
+			() => console.dir(`TaskManager ${task_manager_key} still works!`),
+			{
+				// see https://www.npmjs.com/package/cron for all available
+				start: true,
+				timezone: 'Europe/Moscow',
+				onComplete: () => console.log('a_key_string_to_call_this_job has stopped....'),
+			},
+		)
+		this.exec = exec
+		this.logger = logger
+	}
+
+	public ADD_TASK({ job_key, cron_exp, task }: TaskProps): void {
+		this.manager.add(job_key, cron_exp, () => {
+			this.exec('ADD_TASK', { job_key, cron_exp, task })
+		})
+	}
+
+	public START_TASK({ job_key }: TaskProps): void {
+		this.manager.start(job_key)
+	}
+
+	public STOP_TASK({ job_key }: TaskProps): void {
+		this.manager.stop(job_key)
+	}
+
+	public UPDATE_TASK({ job_key, cron_exp, task }: TaskProps): void {
+		this.manager.update(job_key, cron_exp, () => {
+			this.exec('UPDATE_TASK', { job_key, cron_exp, task })
+		})
+	}
 }
